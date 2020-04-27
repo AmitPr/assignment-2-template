941ef4eba562d4fa9c47936469a1c1320c7aacc2
AmitPr <17amitprasad@gmail.com>
2020-04-26 23:55:01 -0400

    fixes?

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,5 +1,8 @@
 const isRightTriangle = (a, b, c) => {
     let arr = [a*a,b*b,c*c]
+    if(arr.includes(0)){
+        return false
+    }
     arr.sort((a, b) => a - b)
     if(arr[0]+arr[1]==arr[2]){
         return true
